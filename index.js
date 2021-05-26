41209283388cb1e368b45ec1dae2d07c61cde73d
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2021-05-26 11:57:19 -0700

    done!

---
+++ index.js
@@ -0,0 +1,14 @@
+var robotStr = (function(){ /*
+    __
+_(\    |@@|
+(__/\__ \--/ __
+  \___|----|  |   __
+      \ }{ /\ )_ / _\
+      /\__/\ \__O (__
+     (--/\--)    \__/
+     _)(  )(_
+    `---''---`
+  */}).toString().split('\n').slice(1, -1).join('\n');
+
+console.log(robotStr)
+//Wow fancy seeing you here! I assume you liked the robot art. oh btw the next part of the code is "3". and lastly, if you cant find the last code, look up.
